ada59de15b9ea4068fa76c60cd1feb7a3fb92ddf
Hosein110011 <hosein@Hosein.>
2023-11-29 20:02:00 +0330

    add database and create users table

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 	"github.com/go-chi/chi"
 	"net/http"
 	"github.com/go-chi/cors"
+	"github.com/Hosein110011/aggr/internal/database"
+	_ "github.com/lib/pq"
 )
+
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 
 func main() {
@@ -19,6 +26,24 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the enviroment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database: %v", err),
+	}
+
+	queries := database.New(conn)
+	// if err != nil {
+	// 	log.Fatal("Can't create to db connection:", err)
+	// }
+	apiCfg := apiConfig{
+		DB: queries,
 	}
 
 	router := chi.NewRouter()
@@ -36,6 +61,7 @@ func main() {
 	// v1Router.HandleFunc("/healthz", handlerReadiness)
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
